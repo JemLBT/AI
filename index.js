fc1d0ffad8ec5e2d7b1c701e4eb3467713853227
JemLBT <adesignjem@gmail.com>
2025-04-13 14:16:38 +0100

    working

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function generateSmoothie(event) {
-  event.preventDefault();
-  Typewriter("#smoothie", {
+function displaySmoothie(response) {
+  console.log(response.data.answer);
+
+  new Typewriter("#smoothie", {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 1,
   });
+}
 
-  let apiKey = "10588b3do607b336f1e63a30b7f6ft4a";
+function generateSmoothie(event) {
+  event.preventDefault();
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
   let context =
-    "Your are a health concious AI Assistant that generates healthy smoothie reciepes. The reciepe must be provided in HTML format. Example: <p>Here is your smoothie!</p>";
+    "Your are a healthy AI Assistant that gives healthy smoothie recipe. Your recipe's are less than 10 lines. The recipe must be provided in HTML format. Example: <p>this is a recipe</p>";
   let prompt =
-    "Generate a healthy smoothie reciepe. This should be in less than 10 lines. Please use an emoji for each line. ";
+    "Generate a healthy smoothie recipe, in less than 10 lines. Must include fruit. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let smoothieElement = document.querySelector("#smoothie");
 
-  smoothieElement.innerHTML =
-    "Generating a healthy smoothie for you... it will be worth the wait";
+  smoothieElement.innerHTML = "Generating a smoothie for you.. please wait";
 
-  console.log("called the AI api");
   axios.get(apiUrl).then(displaySmoothie);
 }
 
